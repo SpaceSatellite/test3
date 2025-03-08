c8d0e1d8a9ca827b088e64bc1e1952613d61fae4
SpaceSatellite <wolfsimple9@gmail.com>
2025-03-08 14:09:06 +0300

    added new fields in deals statistics, finded bug wich never will fixed, need test new math and create db

--- py_files/test3_1.py
+++ py_files/test3_1.py
@@ -28,7 +28,7 @@ for i in column_names:
     a = column_names.index(i)+1
     b = len(column_names)
     for k in column_names[a:b]:
-        ss_2 = f'create table if not exists {i}_{k}(id_open, id_close, open, high, low, close, profit, clear_profit, profit_p, clear_profit_p)'
+        ss_2 = f'create table if not exists {i}_{k}(id_open, id_close, open, high, low, close, profit, clear_profit, profit_p, clear_profit_p, lowest_delta, lowest_delta_p, lost_income, lost_income_p)'
         cur_2.execute(ss_2)
         con_2.commit()
 
@@ -39,6 +39,8 @@ for i in column_names:
         flag = -1 if (q_part[0][1] - q_part[0][2]) > 0 else 1
 
         for l in q_part:
+            id_point = l[0]
+            current_point = close[id_point]
             n = l[1]-l[2]
             
             if n == 0:
@@ -46,43 +48,65 @@ for i in column_names:
 
             diff = -1 if (n/abs(n)) > 0 else 1
             if diff == flag:
-                if close[l[0]] > deal_high:
-                    deal_high = close[l[0]]
+                if current_point > deal_high:
+                    deal_high = current_point
 
-                if close[l[0]] < deal_low:
-                    deal_low = close[l[0]]
+                if current_point < deal_low:
+                    deal_low = current_point
 
                 continue
 
             if diff != flag:
-                if close[l[0]] > deal_high:
-                    deal_high = close[l[0]]
+                if current_point > deal_high:
+                    deal_high = current_point
 
-                if close[l[0]] < deal_low:
-                    deal_low = close[l[0]]
+                if current_point < deal_low:
+                    deal_low = current_point
 
                 s_2 = f'update {i}_x set {i}_{k} = {flag} where id = {q_full.index(l)}'
                 cur.execute(s_2)
                 con.commit()
 
                 if deal_open == 0:
-                    deal_open = close[l[0]]
-                    id_open = l[0]
+                    deal_open = current_point
+                    deal_low = current_point
+                    deal_high = current_point
+                    id_open = id_point
                 else:
-                    id_close = l[0]
-                    deal_close = close[l[0]]
+                    id_close = id_point
+                    deal_close = current_point
                     deal_profit = deal_open - deal_close if flag == -1 else deal_close - deal_open
                     deal_profit_p = (deal_profit/deal_open)*100
                     deal_clear_profit = deal_profit - (deal_open + deal_close)*0.001
                     deal_clear_profit_p = (deal_clear_profit/deal_open)*100
+                    deal_lowest_delta = deal_low - deal_open if flag == -1 else deal_high - deal_open
+                    deal_lowest_delta_p = (deal_lowest_delta/deal_open)*100 
+                    deal_lost_income = deal_high - deal_close if flag == -1 else deal_low - deal_close
+                    deal_lost_income_p = (deal_close/deal_high)*100 
+
+                    ss_3 = f'insert into {i}_{k} values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
                     
-                    ss_3 = f'insert into {i}_{k} values(?,?,?,?,?,?,?,?,?,?)'
-                    data = [id_open, id_close, deal_open, deal_high, deal_low, deal_close, deal_profit, deal_clear_profit, deal_profit_p, deal_clear_profit_p]
+                    data = [
+                        id_open,                    # id свечи открытия
+                        id_close,                   # id свечи закрытия
+                        deal_open,                  # котировка открытия
+                        deal_high,                  # наивысшая точка в сделке
+                        deal_low,                   # низшая точка в сделке
+                        deal_close,                 # котировка закрытия
+                        deal_profit,                # чистая разница открытия и закрытия
+                        deal_clear_profit,          # прибыль - коммиссия
+                        deal_profit_p,              # прибыль в процентах
+                        deal_clear_profit_p,        # прибыль - коммиссия в процентах
+                        deal_lowest_delta,          # просадка
+                        deal_lowest_delta_p,        # просадка в процентах
+                        deal_lost_income,           # сколько недополучили от максимума
+                        deal_lost_income_p]         # сколько недополучили, но в процентах - эффективность сделки
+                    
                     cur_2.execute(ss_3, data)
                     con_2.commit()
 
-                    deal_open = close[l[0]]
-                    id_open = l[0]
+                    deal_open = current_point
+                    id_open = id_point
                 
                     # id_open = 0
                     id_close = 0
@@ -96,6 +120,9 @@ for i in column_names:
                     deal_clear_profit_p = 0
 
                 flag = diff
+            con.commit()
+            con_2.commit()
+            
 
 con.commit()
 con_2.commit()
